afe19e60dd208935ff570df4d88478af8c05d412
Lourdesmboya <lourdesmboya@gmail.com>
2023-01-18 20:05:09 +0300

    Practice on Networking with Python - Urllib and Requests

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Script that takes in a URL and an email, sends a POST request
+ And displays the body of the response
+"""
 
 import sys
 import urllib.parse
@@ -11,5 +14,5 @@ if __name__ == "__main__":
     data = urllib.parse.urlencode(value).encode("ascii")
 
     request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    with urllib.request.urlopen(request) as resp:
+        print(resp.read().decode("utf-8"))
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
-import sys
+"""
+Script that takes in a URL and an email address, sends a POST request to the passed URL
+with the email as a parameter, and finally displays the body of the response
+"""
 import requests
+from sys import argv
 
-
-if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-
-    r = requests.post(url, data=value)
+if __name__ == '__main__':
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
